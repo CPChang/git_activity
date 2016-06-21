4402a1fbc9e4716c9154a066aa205776afd22f8e
CPChang <chang.pao.chang@gmail.com>
2016-06-20 20:46:22 -0500

    Fix unwanted dropbox traffic

--- gitstats_cmd.bat
+++ gitstats_cmd.bat
@@ -17,6 +17,12 @@
 ::
 :: An additional folder for different git_folder is added, so the files from 
 :: different repository will not be overwritten.
+::
+:: Files will be written in local temp folder first, then copy to dropbox folder
+:: if needed. Instead of delete and write the dropbox folder, which cause twice 
+:: network traffic.  
+:: If not delete the old data, python will have error. `KeyError: 'x'`
+
 
 @echo off
 setlocal
@@ -39,9 +45,10 @@ set gitbashpath="%gitInstallDir:"=%\bin\sh.exe"
 set targetGitRepo=%~f1
 
 
-:: remove the folder if exist and create a new one
-if exist %outputFolder% rmdir /s /q %outputFolder%
-mkdir %outputFolder%
+:: remove the tempFolder if exist and create a new one
+:: later will copy to outputFolder
+if exist %tempFolder% rmdir /s /q %tempFolder%
+mkdir %tempFolder%
 
 :: cd to the folder contain this script
 set thisFolderPath=%~dp0
@@ -51,13 +58,22 @@ cd %thisFolderPath%
 :: need to double the back slash before pass it into bash.
 :: seems like no need to remove the double quote at all
 set python2path_Bash=%python2path:\=\\\\%
-set outputFolder_Bash=%outputFolder:\=\\\\%
+set outputFolder_Bash=%tempFolder:\=\\\\%
 set targetGitRepo_Bash=%targetGitRepo:\=\\\\%
 
 :: call the gitbash
 :: be careful this is the bash syntax
 :: the gitstats_bash.sh takes 3 args: (python2 path),(gitrepo path),(outputPath)
 cmd /c  "%gitbashpath% --login -i -c "bash gitstats_bash.sh %python2path_Bash% %outputFolder_Bash% %targetGitRepo_Bash%""
+
+:: copy to output folder if needed
+:: /C Continues copying even if errors occur.
+:: /L Displays files that would be copied.
+:: /Y Suppresses prompting to confirm you want to overwrite an existing destination file.
+if not "%outputFolder%" == "%tempFolder%" (
+	xcopy %tempFolder%\* %outputFolder% /C /L /Y
+)
+
 
 :: open the webpage
 start "" %outputFolder%\activity.html
